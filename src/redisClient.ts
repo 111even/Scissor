35618e12f6361f34e7b1672ad530e3a74fed3422
Is'haq Abdulrazzaq <ishaqat111@gmail.com>
2024-08-15 16:31:17 +0100

    Port Fix

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -3,7 +3,7 @@ import Redis from 'ioredis';
 // Use environment variables for Redis connection configuration
 const redisClient = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1', // Provide a fallback string
-  port: parseInt(process.env.REDIS_PORT || '6380', 10), // Ensure port is always a number
+  port: parseInt(process.env.REDIS_PORT || '6380', 10), // Ensure port is as always a number
   password: process.env.REDIS_PASSWORD || '', // Provide a fallback string
 });
 

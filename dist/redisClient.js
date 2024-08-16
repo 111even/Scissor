168a2c455abcfdd83907209802e43b7f17586392
Is'haq Abdulrazzaq <ishaqat111@gmail.com>
2024-08-16 14:03:59 +0100

    Modified redisClient.js

--- dist/redisClient.js
+++ dist/redisClient.js
@@ -4,11 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ioredis_1 = __importDefault(require("ioredis"));
-const redisClient = new ioredis_1.default();
+
+const redisClient = new ioredis_1.default({
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: parseInt(process.env.REDIS_PORT || '8000', 10),
+    password: process.env.REDIS_PASSWORD || '',
+});
+
 redisClient.on('connect', () => {
     console.log('Connected to Redis');
 });
+
 redisClient.on('error', (err) => {
     console.error('Redis error:', err);
 });
+
 exports.default = redisClient;
